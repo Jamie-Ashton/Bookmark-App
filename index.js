a31d94654ddc22cffc224a79680ffe3bd0403244
Jamie-Ashton <110884625+Jamie-Ashton@users.noreply.github.com>
2024-09-12 09:26:14 +1200

    update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const modal = document.getElementById('modal');
+const showModal = document.getElementById('show-modal');
 const modalClose = document.getElementById('modal-close');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+})
