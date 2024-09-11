016eda647ff18656efc6b343413628528a26c52a
Jamie-Ashton <110884625+Jamie-Ashton@users.noreply.github.com>
2024-09-12 10:48:20 +1200

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const modal = document.getElementById('modal');
-const showModal = document.getElementById('show-modal');
+const modalShow = document.getElementById('modal-show');
 const modalClose = document.getElementById('modal-close');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
@@ -8,11 +8,38 @@ const bookmarksContainer = document.getElementById('bookmarks-container');
 
 let bookmarks = [];
 
-function showModal() {
-    modal.classList.add('show-modal');
+
+function showModal() { 
+    modal.classList.add('modal-show');
     websiteNameEl.focus();
 }
 
+modalShow.addEventListener('click', modalShow)
 modalClose.addEventListener('click', () => {
-    modal.classList.remove('show-modal');
+    modal.classList.remove('modal-show');
 })
+
+window.addEventListener('click', (e) => {
+    e.target === modal ? modal.classList.remove('modal-show') : false;
+})
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('modal-show');
+})
+
+function validate(nameValue, urlValue) {
+    const expression = 
+    /(https)?:\/\/(www\.)?[-a-zA-z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
+
+    const regex = new RegExp(expression);
+}
+
+if(!nameValue || !urlValue) {
+    alert('Please submit values for both fields');
+    return false;
+}
+
+if(!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+}
